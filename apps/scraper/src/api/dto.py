41d951c26560f7a509c032d688cf406f4b805743
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2025-01-07 01:27:17 +0000

    feat(scraper): added simple api route

---
+++ apps/scraper/src/api/dto.py
@@ -0,0 +1,36 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+from models import Team, TeamStat, Match, Player, PlayerStat
+
+
+class TeamSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Team
+        load_instance = True
+        include_relationships = True
+
+
+class TeamStatsSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = TeamStat
+        load_instance = True
+        include_relationships = True
+
+class MatchSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Match
+        load_instance = True
+        include_relationships = True
+
+
+class PlayerSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Player
+        load_instance = True
+        include_relationships = True
+
+class PlayerStatSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = PlayerStat
+        load_instance = True
+        include_relationships = True
--- apps/scraper/src/api/routes.py
+++ apps/scraper/src/api/routes.py
@@ -1,7 +1,148 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from models import Team, Match, Player, TeamStat, PlayerStat
+from .dto import (
+    TeamSchema,
+    MatchSchema,
+    PlayerSchema,
+    TeamStatsSchema,
+    PlayerStatSchema,
+)
 
-main_bp = Blueprint('main', __name__)
+main_bp = Blueprint("main", __name__)
 
 @main_bp.get("/")
 def hello_world():
     return {"message": "Hello, World!"}
+
+@main_bp.get("/teams")
+def get_teams():
+    teams = main_bp.db_session.query(Team).all()
+    ts = TeamSchema(many=True).dump(teams)
+    return {"teams": ts}
+
+@main_bp.get("/teams/<uuid:team_id>")
+def get_team(team_id):
+    team = main_bp.db_session.query(Team).filter_by(id=team_id).first()
+    return {"team:": TeamSchema().dump(team)}
+
+@main_bp.get("/matches")
+def get_all_matches():
+    limit = request.args.get("limit", default=16, type=int)
+    offset = request.args.get("offset", default=0, type=int)
+    matches = (
+        main_bp.db_session.query(Match)
+        .order_by(Match.played_at)
+        .limit(limit)
+        .offset(offset)
+        .all()
+    )
+    ms = MatchSchema(many=True).dump(matches)
+    return {"matches": ms}
+
+@main_bp.get("/matches/results")
+def get_played_matches():
+    limit = request.args.get("limit", default=16, type=int)
+    offset = request.args.get("offset", default=0, type=int)
+    matches = (
+        main_bp.db_session.query(Match)
+        .filter_by(is_finished=True)
+        .order_by(Match.played_at)
+        .limit(limit)
+        .offset(offset)
+        .all()
+    )
+    ms = MatchSchema(many=True).dump(matches)
+    return {"matches": ms}
+
+@main_bp.get("/matches/fixtures")
+def get_fixtures():
+    limit = request.args.get("limit", default=16, type=int)
+    offset = request.args.get("offset", default=0, type=int)
+    matches = (
+        main_bp.db_session.query(Match)
+        .filter_by(is_finished=False)
+        .order_by(Match.played_at)
+        .limit(limit)
+        .offset(offset)
+        .all()
+    )
+    ms = MatchSchema(many=True).dump(matches)
+    return {"matches": ms}
+
+@main_bp.get("/matches/<uuid:match_id>")
+def get_match(match_id):
+    m = main_bp.db_session.query(Match).filter_by(id=match_id).first()
+    return {"match": MatchSchema().dump(m)}
+
+@main_bp.get("/teams/<uuid:team_id>/matches")
+def get_team_matches(team_id):
+    matches = main_bp.db_session.query(Match).filter(
+        (Match.away_team_id == team_id) | (Match.home_team_id == team_id)
+    ).all()
+    ms = MatchSchema(many=True).dump(matches)
+    return {"matches": ms}
+
+
+@main_bp.get("/teams/<uuid:team_id>/matches/results")
+def get_team_played_matches(team_id):
+    matches = (
+        main_bp.db_session.query(Match)
+        .filter((Match.away_team_id == team_id) | (Match.home_team_id == team_id))
+        .filter_by(is_finished=True)
+        .order_by(Match.played_at)
+        .all()
+    )
+    ms = MatchSchema(many=True).dump(matches)
+    return {"matches": ms}
+
+
+@main_bp.get("/teams/<uuid:team_id>/matches/fixtures")
+def get_team_fixtures(team_id):
+    matches = (
+        main_bp.db_session.query(Match)
+        .filter((Match.away_team_id == team_id) | (Match.home_team_id == team_id))
+        .filter_by(is_finished=False)
+        .order_by(Match.played_at)
+        .all()
+    )
+    ms = MatchSchema(many=True).dump(matches)
+    return {"matches": ms}
+
+@main_bp.get("/players/<uuid:player_id>")
+def get_player(player_id):
+    p = main_bp.db_session.query(Player).filter_by(id=player_id).first()
+    return {"player": PlayerSchema().dump(p)}
+
+@main_bp.get("/players/<uuid:player_id>/stats")
+def get_player_stats(player_id):
+    stats = main_bp.db_session.query(PlayerStat).filter_by(player_id=player_id).all()
+    return {"stats": PlayerStatSchema(many=True).dump(stats)}
+
+@main_bp.get("/teams/<uuid:team_id>/stats")
+def get_team_stats(team_id):
+    stats = main_bp.db_session.query(TeamStat).filter_by(team_id=team_id).all()
+    return {"stats": TeamStatsSchema(many=True).dump(stats)}
+
+@main_bp.get("/stats/players")
+def get_players_stats():
+    limit = request.args.get("limit", default=16, type=int)
+    offset = request.args.get("offset", default=0, type=int)
+    stats = main_bp.db_session.query(PlayerStat).limit(limit).offset(offset).all()
+    return {"stats": PlayerStatSchema(many=True).dump(stats)}
+
+@main_bp.get("/stats/teams")
+def get_teams_stats():
+    limit = request.args.get("limit", default=16, type=int)
+    offset = request.args.get("offset", default=0, type=int)
+    stats = main_bp.db_session.query(TeamStat).limit(limit).offset(offset).all()
+    return {"stats": TeamStatsSchema(many=True).dump(stats)}
+
+@main_bp.get("/stats/teams/<uuid:stat_id>")
+def get_team_stats_by_id(stat_id):
+    stats = main_bp.db_session.query(TeamStat).filter_by(id=stat_id).first()
+    return {"stats": TeamStatsSchema().dump(stats)}
+
+@main_bp.get("/stats/players/<uuid:stat_id>")
+def get_player_stats_by_id(stat_id):
+    stats = main_bp.db_session.query(PlayerStat).filter_by(id=stat_id).first()
+    return {"stats": PlayerStatSchema().dump(stats)}
